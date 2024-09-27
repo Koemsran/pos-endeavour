e10c0248a0e37b8ffed413e3aadd906720ca5fa8
Chan Nich <channichnoeurn@gmail.com>
2024-09-27 16:33:07 +0700

    fix of update permission also icons of action of permission

--- resources/views/setting/permission/index.blade.php
+++ resources/views/setting/permission/index.blade.php
@@ -92,7 +92,7 @@
     <main class="p-4 w-full max-w-xl max-h-full">
       <div class="bg-white rounded-lg shadow">
         <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t">
-          <h3 class="text-xl font-semibold text-gray-900">Edit Slide Show Permissions</h3>
+          <h3 class="text-xl font-semibold text-gray-900">Edit Permissions</h3>
           <button id="closeEditModal" type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8">
             <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
